37cac1f8c977aaabad8995d2a003965cb9621903
syd9538 <syd9538@gmail.com>
2020-07-16 23:04:42 +0900

    리뷰작성 1

--- eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/EatgoApplication.java
@@ -2,8 +2,10 @@ package kr.co.fastcampus.eatgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+
 public class EatgoApplication {
 
     public static void main(String[] args) {
--- eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
+++ eatgo/eatgo-api/src/main/java/kr/co/fastcampus/eatgo/domain/MenuItemRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
+
 public interface MenuItemRepository extends CrudRepository<MenuItem, Long> {
     List<MenuItem> findAllByRestaurantId(Long restaurantId);
 
