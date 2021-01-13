0c795e205b9b55444c49f4e9322e5766c1d050ad
syd9538 <syd9538@gmail.com>
2021-01-13 22:30:11 +0900

    jwt 인증 기능 구현

---
+++ eatgo/eatgo-common/src/main/java/kr/co/fastcampus/eatgo/utils/JwtUtil.java
@@ -0,0 +1,27 @@
+package kr.co.fastcampus.eatgo.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+public class JwtUtil {
+    private Key key;
+
+    public JwtUtil(String secret) {
+        this.key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    public String createToken(long userId, String name) {
+        String secret = "12345678901234567890123456789012";
+        Key key = Keys.hmacShaKeyFor(secret.getBytes());
+        String token = Jwts.builder()
+                .claim("userId", userId)
+                .claim("name", name)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+        return token;
+    }
+}
--- eatgo/eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/SessionControllerTests.java
+++ eatgo/eatgo-customer-api/src/test/java/kr/co/fastcampus/eatgo/interfaces/SessionControllerTests.java
@@ -4,6 +4,7 @@ import kr.co.fastcampus.eatgo.application.EmailNotExistedException;
 import kr.co.fastcampus.eatgo.application.PasswordWrongException;
 import kr.co.fastcampus.eatgo.application.UserService;
 import kr.co.fastcampus.eatgo.domain.User;
+import kr.co.fastcampus.eatgo.utils.JwtUtil;
 import org.h2.engine.Session;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,8 +15,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
+import static org.hamcrest.core.StringContains.containsString;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -28,24 +29,31 @@ class SessionControllerTests {
     MockMvc mvc;
 
     @MockBean
+    private JwtUtil jwtUtil;
+
+    @MockBean
     private UserService userService;
 
     @Test
     public void createWithValidAttributes() throws Exception {
-
+        Long id = 1004L;
+        String name = "Tester";
         String email = "tester@example.com";
         String password = "test";
 
-        User mockUser = User.builder().password("ACCESSTOKEN").build();
+        User mockUser = User.builder().id(id).name(name).build();
 
         given(userService.authenticate(email, password)).willReturn(mockUser);
+
+        given(jwtUtil.createToken(id, name)).willReturn("header.payload.signature");
 
         mvc.perform(post("/session")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content("{\"email\":\"tester@example.com\",\"password\":\"test\"}"))
                     .andExpect(status().isCreated())
                     .andExpect(header().string("location", "/session"))
-                    .andExpect(content().string("{\"accessToken\":\"ACCESSTOKE\"}"));
+                    .andExpect(content().string(containsString("{\"accessToken\":\"header.payload.signature\"}")
+                    ));
 
 
         verify(userService).authenticate(eq(email), eq(password));
